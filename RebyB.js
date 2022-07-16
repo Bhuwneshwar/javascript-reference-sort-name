26931f0958a626611a45aae70fdb77006b704d1a
RebyB Fund <109419048+Rebybfund5@users.noreply.github.com>
2022-07-16 23:05:35 +0530

    short code

--- RebyB.js
+++ RebyB.js
@@ -1,14 +1,14 @@
-const i = (i) => { return document.getElementById(i) }
+const i = i => return document.getElementById(i);
 
-const t = (t) => { return document.getElementsByTagName(t) }
+const t = t => return document.getElementsByTagName(t);
 
-const n = (n) => { return document.getElementsByName(n) }
+const n = n => return document.getElementsByName(n);
 
-const c = (c) => { return document.getElementsByClassName(c) }
+const c = c => return document.getElementsByClassName(c);
 
-const q = (q) => { return document.querySelector(q) }
+const q = q => return document.querySelector(q);
 
-const qA = (qA) => { return document.querySelectorAll(qA) }
+const qA = qA =>  return document.querySelectorAll(qA);
 
-const stopS = (S) => { return i(S).addEventListener('submit', (e) =>  {e.preventDefault() }) }
+const stopS = S =>  return i(S).addEventListener('submit', (e) =>  {e.preventDefault() }) ;
 
